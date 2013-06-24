3c52094abeccea0b8d16ed77787db3efd175292f
Pedro Lula <plopes1991@gmail.com>
2013-06-24 10:55:08 -0300

    Criando o Dashboard

--- app/controllers/local_controller.rb
+++ app/controllers/local_controller.rb
@@ -3,5 +3,13 @@ class LocalController < ApplicationController
   end
 
   def show
+    fs = Foursquare2::Client.new(:client_id => '5XO4RX3ADLYLERXE2M25HH3KJ0UKQ5OER14MNHMYIMC31CUE', :client_secret => 'ZEQ2TZA1P0OC1R1G5DQGQCQGM1EO0P4JMGH4QEPBZDTT5OCA')
+    venue_id = params[:id].to_s
+    @local = fs.venue(venue_id)
+    @similar = fs.search_venues_by_tip(:near => 'Belo Horizonte', :query => @local.name, :categoryId => @local.categories.first.id, :limit=>'5')
+    respond_to do |f|
+      f.json 
+      f.html
+    end
   end
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
     
   def show
     fs = Foursquare2::Client.new(:client_id => '5XO4RX3ADLYLERXE2M25HH3KJ0UKQ5OER14MNHMYIMC31CUE', :client_secret => 'ZEQ2TZA1P0OC1R1G5DQGQCQGM1EO0P4JMGH4QEPBZDTT5OCA')
-    @local = fs.search_venues_by_tip(:near => 'Belo Horizonte', :query => params[:lq], :limit=>'5')
+    @local = fs.search_venues_by_tip(:near => 'Belo Horizonte', :query => params[:lq], :limit=>'10')
     respond_to do |f|
       f.json 
       f.html
