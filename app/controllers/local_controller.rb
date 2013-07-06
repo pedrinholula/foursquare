9ef1377a2c1ac3629f143492bbc5144fb54f3c43
Pedro Lula <plopes1991@gmail.com>
2013-07-05 21:44:44 -0300

    Similar controller done

--- app/controllers/local_controller.rb
+++ app/controllers/local_controller.rb
@@ -3,16 +3,18 @@ class LocalController < ApplicationController
   end
 
   def show
+    #Inicia o foursquare e faz a pesquisa do local e das fotos do mesmo
     fs = Foursquare2::Client.new(:client_id => '5XO4RX3ADLYLERXE2M25HH3KJ0UKQ5OER14MNHMYIMC31CUE', :client_secret => 'ZEQ2TZA1P0OC1R1G5DQGQCQGM1EO0P4JMGH4QEPBZDTT5OCA')
     flash[:venue_id] = params[:id]
-    @local = fs.venue(params[:id])
-    @venue_photo = fs.venue_photos(params[:id], :group => "checkin", :group => "venue", :limit => 20)
+    @local = fs.venue(params[:id]) #Busca dados do Local
+    @venue_photo = fs.venue_photos(params[:id], :group => "checkin", :group => "venue", :limit => 20) #Busca fotos do local 
     respond_to do |f|
       f.html
     end
   end
   
   def photos
+    #Necessário para os dados das photos
     fs = Foursquare2::Client.new(:client_id => '5XO4RX3ADLYLERXE2M25HH3KJ0UKQ5OER14MNHMYIMC31CUE', :client_secret => 'ZEQ2TZA1P0OC1R1G5DQGQCQGM1EO0P4JMGH4QEPBZDTT5OCA')
     venue_id = flash[:venue_id]
     flash[:venue_id] = venue_id
@@ -36,6 +38,7 @@ class LocalController < ApplicationController
   end
   
   def tips
+    fs = Foursquare2::Client.new(:client_id => '5XO4RX3ADLYLERXE2M25HH3KJ0UKQ5OER14MNHMYIMC31CUE', :client_secret => 'ZEQ2TZA1P0OC1R1G5DQGQCQGM1EO0P4JMGH4QEPBZDTT5OCA')
     venue_id = flash[:venue_id]
     flash[:venue_id] = venue_id
     tip = fs.venue_tips(venue_id,:sort => :popular, :limit => 1000)
@@ -59,19 +62,25 @@ class LocalController < ApplicationController
   end
 
   def similar
+    #gera os valores de similaridade
+    fs = Foursquare2::Client.new(:client_id => '5XO4RX3ADLYLERXE2M25HH3KJ0UKQ5OER14MNHMYIMC31CUE', :client_secret => 'ZEQ2TZA1P0OC1R1G5DQGQCQGM1EO0P4JMGH4QEPBZDTT5OCA')
     venue_id = flash[:venue_id]
     flash[:venue_id] = venue_id
     @local = fs.venue(venue_id)
     similar = fs.search_venues_by_tip(:near => 'Belo Horizonte', :query => @local.categories.first.name, :categoryId => @local.categories.first.id, :intent => "match")
     
-    @similar={}
+    s={}
     similar.each do |e|
-      if !@similar.has_key? e.name
+      if !s.has_key? e.name
         if e.categories.first.id == @local.categories.first.id
-          @similar[e.name] = {:stats => e.stats, :venue_likes => e.likes}
-          @similar[e.name] = {:stats => e.stats, :venue_likes => e.likes}
+          s[e.name] = {:id => e.id, :stats => e.stats, :venue_likes => e.likes}
         end
       end
+    end
+    
+    @similar = []
+    s.each do |key, val|
+      @similar.push({:name => key}.update(val))
     end
     respond_to do |f|
       f.json
